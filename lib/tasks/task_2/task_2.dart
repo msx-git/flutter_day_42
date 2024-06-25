b136bd79dc06c26f501cb3f8990d2ae328a21cd1
Musoxon <msx.tuit@gmail.com>
2024-06-25 09:04:59 +0500

    Task is done.

--- lib/tasks/task_2/task_2.dart
+++ lib/tasks/task_2/task_2.dart
@@ -7,8 +7,28 @@ class Task2 extends StatefulWidget {
   State<Task2> createState() => _Task2State();
 }
 
-class _Task2State extends State<Task2> {
+class _Task2State extends State<Task2> with SingleTickerProviderStateMixin{
+
   final pageController = PageController();
+  late final AnimationController _animationController;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _animation = Tween<double>(
+      begin: 0,
+      end: 400,
+    ).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+    // _animationController.forward();
+    _animationController.repeat();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,34 +38,40 @@ class _Task2State extends State<Task2> {
         backgroundColor: Colors.black,
         foregroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 400,
-            child: PageView.builder(
-              controller: pageController,
-              // itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: const EdgeInsets.all(10),
-                  margin: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(color: Colors.teal),
-                  alignment: Alignment.center,
-                  child: Text("Page $index"),
-                );
-              },
-            ),
-          ),
-          IconButton(
-            onPressed: () {
-              pageController.nextPage(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-              );
-            },
-            icon: const Icon(Icons.navigate_next),
-          )
-        ],
+      body: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+          return Column(
+            children: [
+              SizedBox(
+                height: 400,
+                child: PageView.builder(
+                  controller: pageController,
+                  // itemCount: 10,
+                  itemBuilder: (context, index) {
+                    pageController.nextPage(duration: Duration(seconds: 3), curve: Curves.linear);
+                    return Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(color: Colors.teal),
+                      alignment: Alignment.center,
+                      child: Text("Page $index",style: TextStyle(color: Colors.white),),
+                    );
+                  },
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                icon: const Icon(Icons.navigate_next),
+              )
+            ],
+          );
+        }
       ),
     );
   }
